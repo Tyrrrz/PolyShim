68526fa620c75852de9074969f5ac982a18df51d
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-04-05 12:59:15 +0300

    Add tests for `RuntimeInformation`

---
+++ PolyShim.Tests/NetCore10/RuntimeInformationTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore10;
+
+public class RuntimeInformationTests
+{
+    [Fact]
+    public void IsOsPlatform_Test()
+    {
+        // Act
+        var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+        var isOsx = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        // Assert
+#if PLATFORM_WINDOWS
+        isWindows.Should().BeTrue();
+        isLinux.Should().BeFalse();
+        isOsx.Should().BeFalse();
+        #elif PLATFORM_LINUX
+        isWindows.Should().BeFalse();
+        isLinux.Should().BeTrue();
+        isOsx.Should().BeFalse();
+#elif PLATFORM_OSX
+        isWindows.Should().BeFalse();
+        isLinux.Should().BeFalse();
+        isOsx.Should().BeTrue();
+#else
+        isWindows.Should().BeFalse();
+        isLinux.Should().BeFalse();
+        isOsx.Should().BeFalse();
+#endif
+    }
+}
