3abb1ac54f3fb92f17cba79d7c96d4c242b2a8e5
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-04-14 21:03:26 +0300

    Add polyfills for `Task.WaitAsync(...)`

---
+++ PolyShim.Tests/Net60/TaskTests.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net60;
+
+public class TaskTests
+{
+    [Fact]
+    public async Task WaitAsync_NoResult_Negative_ByTokenToken_Positive_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+        var task = Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+
+        // Act & assert
+        await task.WaitAsync(cts.Token);
+    }
+
+    [Fact]
+    public async Task WaitAsync_NoResult_Negative_ByTokenToken_Negative_ByToken_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(0.01));
+        var task = Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+
+        // Act
+        var ex = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () =>
+            await task.WaitAsync(cts.Token)
+        );
+
+        // Assert
+        ex.CancellationToken.Should().Be(cts.Token);
+    }
+
+    [Fact]
+    public async Task WaitAsync_NoResult_TimeSpan_Positive_Test()
+    {
+        // Arrange
+        var task = Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+
+        // Act & assert
+        await task.WaitAsync(TimeSpan.FromSeconds(1));
+    }
+
+    [Fact]
+    public async Task WaitAsync_NoResult_TimeSpan_Negative_ByTimeout_Test()
+    {
+        // Arrange
+        var task = Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+
+        // Act & assert
+        await Assert.ThrowsAnyAsync<TimeoutException>(async () =>
+            await task.WaitAsync(TimeSpan.FromSeconds(0.01))
+        );
+    }
+
+    [Fact]
+    public async Task WaitAsync_NoResult_TimeSpanAndCancellationToken_Positive_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+        var task = Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+
+        // Act & assert
+        await task.WaitAsync(TimeSpan.FromSeconds(1), cts.Token);
+    }
+
+    [Fact]
+    public async Task WaitAsync_NoResult_TimeSpanAndCancellationToken_Negative_ByToken_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(0.01));
+        var task = Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+
+        // Act
+        var ex = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () =>
+            await task.WaitAsync(TimeSpan.FromSeconds(1), cts.Token)
+        );
+
+        // Assert
+        ex.CancellationToken.Should().Be(cts.Token);
+    }
+
+    [Fact]
+    public async Task WaitAsync_NoResult_TimeSpanAndCancellationToken_Negative_ByTimeout_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+        var task = Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+
+        // Act & assert
+        await Assert.ThrowsAnyAsync<TimeoutException>(async () =>
+            await task.WaitAsync(TimeSpan.FromSeconds(0.01), cts.Token)
+        );
+    }
+
+    [Fact]
+    public async Task WaitAsync_WithResult_Negative_ByTokenToken_Positive_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+            return 42;
+        }, CancellationToken.None);
+
+        // Act
+        var result = await task.WaitAsync(cts.Token);
+
+        // Assert
+        result.Should().Be(42);
+    }
+
+    [Fact]
+    public async Task WaitAsync_WithResult_Negative_ByTokenToken_Negative_ByToken_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(0.01));
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+            return 42;
+        }, CancellationToken.None);
+
+        // Act
+        var ex = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () =>
+            await task.WaitAsync(cts.Token)
+        );
+
+        // Assert
+        ex.CancellationToken.Should().Be(cts.Token);
+    }
+
+    [Fact]
+    public async Task WaitAsync_WithResult_TimeSpan_Positive_Test()
+    {
+        // Arrange
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+            return 42;
+        }, CancellationToken.None);
+
+        // Act
+        var result = await task.WaitAsync(TimeSpan.FromSeconds(1));
+
+        // Assert
+        result.Should().Be(42);
+    }
+
+    [Fact]
+    public async Task WaitAsync_WithResult_TimeSpan_Negative_ByTimeout_Test()
+    {
+        // Arrange
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+            return 42;
+        }, CancellationToken.None);
+
+        // Act & assert
+        await Assert.ThrowsAnyAsync<TimeoutException>(async () =>
+            await task.WaitAsync(TimeSpan.FromSeconds(0.01))
+        );
+    }
+
+    [Fact]
+    public async Task WaitAsync_WithResult_TimeSpanAndCancellationToken_Positive_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+            return 42;
+        }, CancellationToken.None);
+
+        // Act
+        var result = await task.WaitAsync(TimeSpan.FromSeconds(1), cts.Token);
+
+        // Assert
+        result.Should().Be(42);
+    }
+
+    [Fact]
+    public async Task WaitAsync_WithResult_TimeSpanAndCancellationToken_Negative_ByToken_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(0.01));
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+            return 42;
+        }, CancellationToken.None);
+
+        // Act
+        var ex = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () =>
+            await task.WaitAsync(TimeSpan.FromSeconds(1), cts.Token)
+        );
+
+        // Assert
+        ex.CancellationToken.Should().Be(cts.Token);
+    }
+
+    [Fact]
+    public async Task WaitAsync_WithResult_TimeSpanAndCancellationToken_Negative_ByTimeout_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(1));
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1), CancellationToken.None);
+            return 42;
+        }, CancellationToken.None);
+
+        // Act & assert
+        await Assert.ThrowsAnyAsync<TimeoutException>(async () =>
+            await task.WaitAsync(TimeSpan.FromSeconds(0.01), cts.Token)
+        );
+    }
+}
---
+++ PolyShim/Net60/Task.cs
@@ -0,0 +1,62 @@
+﻿#if FEATURE_TASK
+#if (NETCOREAPP && !NET6_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+
+[ExcludeFromCodeCoverage]
+internal static class _D314682FE30A40479B425C42FF0A3E9C
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync#system-threading-tasks-task-waitasync(system-timespan-system-threading-cancellationtoken)
+    public static async Task WaitAsync(this Task task, TimeSpan timeout, CancellationToken cancellationToken)
+    {
+        var cancellationTask = Task.Delay(timeout, cancellationToken);
+        var finishedTask = await Task.WhenAny(task, cancellationTask).ConfigureAwait(false);
+
+        // Finalize and propagate exceptions
+        await finishedTask.ConfigureAwait(false);
+
+        if (finishedTask == cancellationTask)
+            throw new TimeoutException("The operation has timed out.");
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync#system-threading-tasks-task-waitasync(system-threading-cancellationtoken)
+    public static async Task WaitAsync(this Task task, CancellationToken cancellationToken) =>
+        await task.WaitAsync(Timeout.InfiniteTimeSpan, cancellationToken).ConfigureAwait(false);
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task.waitasync#system-threading-tasks-task-waitasync(system-timespan)
+    public static async Task WaitAsync(this Task task, TimeSpan timeout) =>
+        await task.WaitAsync(timeout, CancellationToken.None).ConfigureAwait(false);
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.waitasync#system-threading-tasks-task-1-waitasync(system-timespan-system-threading-cancellationtoken)
+    public static async Task<T> WaitAsync<T>(this Task<T> task, TimeSpan timeout, CancellationToken cancellationToken)
+    {
+        var cancellationTask = Task.Delay(timeout, cancellationToken);
+        var finishedTask = await Task.WhenAny(task, cancellationTask).ConfigureAwait(false);
+
+        // Finalize and propagate exceptions
+        await finishedTask.ConfigureAwait(false);
+
+        if (finishedTask == cancellationTask)
+            throw new TimeoutException("The operation has timed out.");
+
+        // If the exception is not thrown, we can safely assume that the main task is completed
+        return task.Result;
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.waitasync#system-threading-tasks-task-1-waitasync(system-threading-cancellationtoken)
+    public static async Task<T> WaitAsync<T>(this Task<T> task, CancellationToken cancellationToken) =>
+        await task.WaitAsync(Timeout.InfiniteTimeSpan, cancellationToken).ConfigureAwait(false);
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.threading.tasks.task-1.waitasync#system-threading-tasks-task-1-waitasync(system-timespan)
+    public static async Task<T> WaitAsync<T>(this Task<T> task, TimeSpan timeout) =>
+        await task.WaitAsync(timeout, CancellationToken.None).ConfigureAwait(false);
+}
+#endif
+#endif
