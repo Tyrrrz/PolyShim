8c13c795e8ce2ceaa5bd87a63ddfb8a2666af67f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-05-16 07:26:20 +0300

    Add `LibraryImportAttribute`

---
+++ PolyShim/Net70/LibraryImportAttribute.cs
@@ -0,0 +1,32 @@
+﻿#if (NETCOREAPP && !NET7_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Runtime.InteropServices;
+
+// Unfortunately, the source generator that uses this attribute is only available on .NET 7 and above
+// https://github.com/dotnet/runtime/issues/60595#issuecomment-1111086988
+
+// https://learn.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.libraryimportattribute
+[AttributeUsage(AttributeTargets.Method, Inherited = false)]
+[ExcludeFromCodeCoverage]
+internal class LibraryImportAttribute : Attribute
+{
+    public string LibraryName { get; }
+
+    public string? EntryPoint { get; init; }
+
+    public bool SetLastError { get; init; }
+
+    public StringMarshalling StringMarshalling { get; init; }
+
+    public Type? StringMarshallingCustomType { get; init; }
+
+    public LibraryImportAttribute(string libraryName) =>
+        LibraryName = libraryName;
+}
+#endif
---
+++ PolyShim/Net70/StringMarshalling.cs
@@ -0,0 +1,18 @@
+﻿#if (NETCOREAPP && !NET7_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Runtime.InteropServices;
+
+// https://learn.microsoft.com/en-us/dotnet/api/system.runtime.interopservices.stringmarshalling
+internal enum StringMarshalling
+{
+    Custom = 0,
+    Utf8,
+    Utf16
+}
+#endif
