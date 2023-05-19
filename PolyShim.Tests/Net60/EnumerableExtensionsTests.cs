17240f35c41e3fc9cfa4d3589af472b51fdbbfe3
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-05-19 03:29:45 +0300

    Add `MinBy(...)` and `MaxBy(...)` polyfills

---
+++ PolyShim.Tests/Net60/EnumerableExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net60;
+
+public class EnumerableExtensionsTests
+{
+    [Fact]
+    public void MinBy_Test()
+    {
+        // Arrange
+        var source = new[]
+        {
+            new KeyValuePair<string, int>("Foo", 42),
+            new KeyValuePair<string, int>("Bar", 13),
+            new KeyValuePair<string, int>("Baz", 69),
+            new KeyValuePair<string, int>("Qux", 17)
+        };
+
+        // Act
+        var result = source.MinBy(x => x.Value);
+
+        // Assert
+        result.Key.Should().Be("Bar");
+        result.Value.Should().Be(13);
+    }
+
+    [Fact]
+    public void MaxBy_Test()
+    {
+        // Arrange
+        var source = new[]
+        {
+            new KeyValuePair<string, int>("Foo", 42),
+            new KeyValuePair<string, int>("Bar", 13),
+            new KeyValuePair<string, int>("Baz", 69),
+            new KeyValuePair<string, int>("Qux", 17)
+        };
+
+        // Act
+        var result = source.MaxBy(x => x.Value);
+
+        // Assert
+        result.Key.Should().Be("Baz");
+        result.Value.Should().Be(69);
+    }
+}
---
+++ PolyShim/Net60/EnumerableExtensions.cs
@@ -0,0 +1,41 @@
+﻿#if (NETCOREAPP && !NET6_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Linq;
+
+[ExcludeFromCodeCoverage]
+internal static class _5F37AE3F29344B63A302CBE9D66BBA2D
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.minby#system-linq-enumerable-minby-2(system-collections-generic-ienumerable((-0))-system-func((-0-1))-system-collections-generic-icomparer((-1)))
+    public static T? MinBy<T, TKey>(
+        this IEnumerable<T> source,
+        Func<T, TKey> keySelector,
+        IComparer<TKey> comparer) =>
+        source.OrderBy(keySelector, comparer).FirstOrDefault();
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.minby#system-linq-enumerable-minby-2(system-collections-generic-ienumerable((-0))-system-func((-0-1)))
+    public static T? MinBy<T, TKey>(
+        this IEnumerable<T> source,
+        Func<T, TKey> keySelector) =>
+        source.MinBy(keySelector, Comparer<TKey>.Default);
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.maxby#system-linq-enumerable-maxby-2(system-collections-generic-ienumerable((-0))-system-func((-0-1))-system-collections-generic-icomparer((-1)))
+    public static T? MaxBy<T, TKey>(
+        this IEnumerable<T> source,
+        Func<T, TKey> keySelector,
+        IComparer<TKey> comparer) =>
+        source.OrderByDescending(keySelector, comparer).FirstOrDefault();
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.maxby#system-linq-enumerable-maxby-2(system-collections-generic-ienumerable((-0))-system-func((-0-1)))
+    public static T? MaxBy<T, TKey>(
+        this IEnumerable<T> source,
+        Func<T, TKey> keySelector) =>
+        source.MaxBy(keySelector, Comparer<TKey>.Default);
+}
+#endif
