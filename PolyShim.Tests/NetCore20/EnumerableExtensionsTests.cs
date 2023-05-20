870d5255e4854dde21d20bcda29c155608eb80ba
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-05-20 07:59:59 +0300

    Add `TakeLast(...)` and `SkipLast(...)`

--- PolyShim.Tests/NetCore20/EnumerableExtensionsTests.cs
+++ PolyShim.Tests/NetCore20/EnumerableExtensionsTests.cs
@@ -7,6 +7,32 @@ namespace PolyShim.Tests.NetCore20;
 public class EnumerableExtensionsTests
 {
     [Fact]
+    public void TakeLast_Test()
+    {
+        // Arrange
+        var source = new[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        var result = source.TakeLast(3);
+
+        // Assert
+        result.Should().Equal(3, 4, 5);
+    }
+
+    [Fact]
+    public void SkipLast_Test()
+    {
+        // Arrange
+        var source = new[] { 1, 2, 3, 4, 5 };
+
+        // Act
+        var result = source.SkipLast(3);
+
+        // Assert
+        result.Should().Equal(1, 2);
+    }
+
+    [Fact]
     public void ToHashSet_Test()
     {
         // Arrange
--- PolyShim/NetCore20/EnumerableExtensions.cs
+++ PolyShim/NetCore20/EnumerableExtensions.cs
@@ -1,4 +1,4 @@
-﻿#if (NETCOREAPP && !NETCOREAPP2_0_OR_GREATER) || (NET35_OR_GREATER && !NET472_OR_GREATER) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
+﻿#if (NETCOREAPP && !NETCOREAPP2_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
 #nullable enable
 // ReSharper disable RedundantUsingDirective
 // ReSharper disable CheckNamespace
@@ -11,6 +11,15 @@ namespace System.Linq;
 
 internal static partial class PolyfillExtensions
 {
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.takelast
+    public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int count) =>
+        source.Reverse().Take(count).Reverse();
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.skiplast
+    public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> source, int count) =>
+        source.Reverse().Skip(count).Reverse();
+
+#if !NET472_OR_GREATER
     // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.tohashset#system-linq-enumerable-tohashset-1(system-collections-generic-ienumerable((-0))-system-collections-generic-iequalitycomparer((-0)))
     public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer) =>
         new(source, comparer);
@@ -18,5 +27,6 @@ internal static partial class PolyfillExtensions
     // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.tohashset#system-linq-enumerable-tohashset-1(system-collections-generic-ienumerable((-0)))
     public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source) =>
         source.ToHashSet(EqualityComparer<T>.Default);
+#endif
 }
 #endif
