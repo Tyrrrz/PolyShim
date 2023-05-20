c76f43afef4d82f4a2be6a8d0226efff4c33c056
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-05-20 07:26:44 +0300

    Add `Order()` and `OrderDescending()`

---
+++ PolyShim.Tests/Net70/EnumerableExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net70;
+
+public class EnumerableExtensionsTests
+{
+    [Fact]
+    public void Order_Test()
+    {
+        // Arrange
+        var source = new[] { 42, 13, 69, 17 };
+
+        // Act
+        var result = source.Order();
+
+        // Assert
+        result.Should().Equal(13, 17, 42, 69);
+    }
+
+    [Fact]
+    public void OrderDescending_Test()
+    {
+        // Arrange
+        var source = new[] { 42, 13, 69, 17 };
+
+        // Act
+        var result = source.OrderDescending();
+
+        // Assert
+        result.Should().Equal(69, 42, 17, 13);
+    }
+}
---
+++ PolyShim/Net70/EnumerableExtensions.cs
@@ -0,0 +1,30 @@
+﻿#if (NETCOREAPP && !NET7_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System.Collections.Generic;
+
+namespace System.Linq;
+
+internal static partial class PolyfillExtensions
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.order#system-linq-enumerable-order-1(system-collections-generic-ienumerable((-0))-system-collections-generic-icomparer((-0)))
+    internal static IOrderedEnumerable<T> Order<T>(this IEnumerable<T> source, IComparer<T> comparer) =>
+        source.OrderBy(x => x, comparer);
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.order#system-linq-enumerable-order-1(system-collections-generic-ienumerable((-0)))
+    internal static IOrderedEnumerable<T> Order<T>(this IEnumerable<T> source) =>
+        source.Order(Comparer<T>.Default);
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.orderdescending#system-linq-enumerable-orderdescending-1(system-collections-generic-ienumerable((-0))-system-collections-generic-icomparer((-0)))
+    internal static IOrderedEnumerable<T> OrderDescending<T>(this IEnumerable<T> source, IComparer<T> comparer) =>
+        source.OrderByDescending(x => x, comparer);
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.orderdescending#system-linq-enumerable-orderdescending-1(system-collections-generic-ienumerable((-0)))
+    internal static IOrderedEnumerable<T> OrderDescending<T>(this IEnumerable<T> source) =>
+        source.OrderDescending(Comparer<T>.Default);
+}
+#endif
