1e6c90356c39a3ca173bdc5d6b508b1cc6aefdf5
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-05-22 12:15:29 +0300

    Add `StringSyntaxAttribute`

---
+++ PolyShim.Tests/Net70/StringSyntaxAttributeTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net70;
+
+public class StringSyntaxAttributeTests
+{
+    // The parameter should be highlighted at the call site
+    private static Regex CreateRegex(
+        [StringSyntax(StringSyntaxAttribute.Regex)]
+        string pattern
+    ) => new(pattern);
+
+    [Fact]
+    public void Initialization_Test()
+    {
+        // Act
+        var regex = CreateRegex(@"foo\sbar");
+
+        // Assert
+        regex.IsMatch("foo bar").Should().BeTrue();
+    }
+}
---
+++ PolyShim/Net70/StringSyntaxAttribute.cs
@@ -0,0 +1,61 @@
+﻿#if (NETCOREAPP && !NET7_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+namespace System.Diagnostics.CodeAnalysis;
+
+// https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis.stringsyntaxattribute
+[AttributeUsage(
+    AttributeTargets.Field |
+    AttributeTargets.Parameter |
+    AttributeTargets.Property
+)]
+[ExcludeFromCodeCoverage]
+internal partial class StringSyntaxAttribute : Attribute
+{
+    public string Syntax { get; }
+
+    public object?[] Arguments { get; }
+
+    public StringSyntaxAttribute(string syntax, params object?[] arguments)
+    {
+        Syntax = syntax;
+        Arguments = arguments;
+    }
+
+    public StringSyntaxAttribute(string syntax)
+        : this(syntax, new object[0])
+    {
+    }
+}
+
+internal partial class StringSyntaxAttribute
+{
+    public const string CompositeFormat = nameof(CompositeFormat);
+
+    public const string DateOnlyFormat = nameof(DateOnlyFormat);
+
+    public const string DateTimeFormat = nameof(DateTimeFormat);
+
+    public const string EnumFormat = nameof(EnumFormat);
+
+    public const string GuidFormat = nameof(GuidFormat);
+
+    public const string Json = nameof(Json);
+
+    public const string NumericFormat = nameof(NumericFormat);
+
+    public const string Regex = nameof(Regex);
+
+    public const string TimeOnlyFormat = nameof(TimeOnlyFormat);
+
+    public const string TimeSpanFormat = nameof(TimeSpanFormat);
+
+    public const string Uri = nameof(Uri);
+
+    public const string Xml = nameof(Xml);
+}
+#endif
