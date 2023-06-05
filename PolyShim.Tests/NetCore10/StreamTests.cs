c059d8f63b981ea331ead87f37fa91500f55e3d2
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-06-05 19:42:59 +0300

    Add `Stream.CopyTo(...)`

---
+++ PolyShim.Tests/NetCore10/StreamTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore10;
+
+public class StreamTests
+{
+    [Fact]
+    public void CopyTo_Test()
+    {
+        // Arrange
+        using var source = new MemoryStream(new byte[] { 1, 2, 3, 4, 5 });
+        using var destination = new MemoryStream();
+
+        // Act
+        source.CopyTo(destination);
+
+        // Assert
+        destination.ToArray().Should().Equal(1, 2, 3, 4, 5);
+    }
+}
---
+++ PolyShim/NetCore10/Stream.cs
@@ -0,0 +1,30 @@
+﻿#if (NETFRAMEWORK && !NET40_OR_GREATER)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System.IO;
+
+internal static partial class PolyfillExtensions
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.io.stream.copyto#system-io-stream-copyto(system-io-stream-system-int32)
+    public static void CopyTo(this Stream source, Stream destination, int bufferSize)
+    {
+        var buffer = new byte[bufferSize];
+        while (true)
+        {
+            var bytesRead = source.Read(buffer, 0, buffer.Length);
+            if (bytesRead <= 0)
+                break;
+
+            destination.Write(buffer, 0, bytesRead);
+        }
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.io.stream.copyto#system-io-stream-copyto(system-io-stream)
+    public static void CopyTo(this Stream source, Stream destination) =>
+        source.CopyTo(destination, 81920);
+}
+#endif
