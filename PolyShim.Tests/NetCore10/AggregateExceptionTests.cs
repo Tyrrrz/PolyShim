8e13dc03e79d1ee11a0ec7520b4ec4e2c482ffc1
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-06-05 20:03:32 +0300

    Add `AggregateException`

---
+++ PolyShim.Tests/NetCore10/AggregateExceptionTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore10;
+
+public class AggregateExceptionTests
+{
+    [Fact]
+    public void Throw_Test()
+    {
+        // Arrange
+        var exception = new AggregateException(
+            new Exception("Exception 1"),
+            new Exception("Exception 2"),
+            new Exception("Exception 3")
+        );
+
+        // Act
+        var result = Assert.Throws<AggregateException>(void () => throw exception);
+
+        // Assert
+        result.Message.Should().Contain("One or more errors occurred");
+        result.InnerExceptions[0].Message.Should().Be("Exception 1");
+        result.InnerExceptions[1].Message.Should().Be("Exception 2");
+        result.InnerExceptions[2].Message.Should().Be("Exception 3");
+    }
+
+    [Fact]
+    public void Flatten_Test()
+    {
+        // Arrange
+        var exception = new AggregateException(
+            new AggregateException(
+                new Exception("Exception 1"),
+                new Exception("Exception 2")
+            ),
+            new Exception("Exception 3")
+        );
+
+        // Act
+        var result = exception.Flatten();
+
+        // Assert
+        result.InnerExceptions.Select(e => e.Message).Should().BeEquivalentTo(
+            "Exception 1",
+            "Exception 2",
+            "Exception 3"
+        );
+    }
+
+    [Fact]
+    public void Handle_Test()
+    {
+        // Arrange
+        var exception = new AggregateException(
+            new Exception("Exception 1"),
+            new Exception("Exception 2"),
+            new Exception("Exception 3")
+        );
+
+        // Act
+        var result = Assert.Throws<AggregateException>(void () =>
+            exception.Handle(ex => ex.Message == "Exception 2")
+        );
+
+        // Assert
+        result.InnerExceptions.Select(e => e.Message).Should().BeEquivalentTo(
+            "Exception 1",
+            "Exception 3"
+        );
+    }
+}
---
+++ PolyShim/NetCore10/AggregateException.cs
@@ -0,0 +1,119 @@
+﻿#if (NETFRAMEWORK && !NET40_OR_GREATER)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace System;
+
+// https://learn.microsoft.com/en-us/dotnet/api/system.aggregateexception
+[ExcludeFromCodeCoverage]
+public class AggregateException : Exception
+{
+    public ReadOnlyCollection<Exception> InnerExceptions { get; } = new(new Exception[0]);
+
+    public AggregateException(string? message, params Exception[] innerExceptions)
+        : base(message, innerExceptions.FirstOrDefault())
+    {
+        InnerExceptions = new ReadOnlyCollection<Exception>(innerExceptions);
+    }
+
+    public AggregateException(string? message, IEnumerable<Exception> innerExceptions)
+        : this(message, innerExceptions.ToArray())
+    {
+    }
+
+    public AggregateException(string? message, Exception innerException)
+        : this(message, new[] {innerException})
+    {
+    }
+
+    public AggregateException(params Exception[] innerExceptions)
+        : this("One or more errors occurred.", innerExceptions)
+    {
+    }
+
+    public AggregateException(IEnumerable<Exception> innerExceptions)
+        : this("One or more errors occurred.", innerExceptions)
+    {
+    }
+
+    public AggregateException(string? message)
+        : this(message, new Exception[0])
+    {
+    }
+
+    public AggregateException()
+        : this("One or more errors occurred.", new Exception[0])
+    {
+    }
+
+    public AggregateException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+    }
+
+    public AggregateException Flatten()
+    {
+        var innerExceptions = new List<Exception>();
+
+        foreach (var exception in InnerExceptions)
+        {
+            if (exception is AggregateException aggregateException)
+            {
+                innerExceptions.AddRange(aggregateException.Flatten().InnerExceptions);
+            }
+            else
+            {
+                innerExceptions.Add(exception);
+            }
+        }
+
+        return new AggregateException(innerExceptions);
+    }
+
+    public void Handle(Func<Exception, bool> predicate)
+    {
+        foreach (var exception in InnerExceptions)
+        {
+            if (exception is AggregateException aggregateException)
+            {
+                aggregateException.Handle(predicate);
+            }
+            else if (predicate(exception))
+            {
+                throw exception;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        var text = base.ToString();
+
+        for (var i = 0; i < InnerExceptions.Count; i++)
+        {
+            text = string.Format(
+                CultureInfo.InvariantCulture,
+                "{0}{1}---> (Inner Exception #{2}) {3}{4}{5}",
+                text,
+                Environment.NewLine,
+                i,
+                InnerExceptions[i].ToString(),
+                "<---",
+                Environment.NewLine
+            );
+        }
+
+        return text;
+    }
+}
+#endif
