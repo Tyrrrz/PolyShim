65043b61a270fbc9493e6fbf3b1c4c0c01fe6264
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-08-07 20:14:11 +0300

    Add `DictionaryEntry.Deconstruct(...)`

---
+++ PolyShim.Tests/NetCore20/DictionaryEntryTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore20;
+
+public class DictionaryEntryTests
+{
+    [Fact]
+    public void Deconstruct_Test()
+    {
+        // Arrange
+        var pair = new DictionaryEntry("hello world", 42);
+
+        // Act
+        var (key, value) = pair;
+
+        // Assert
+        key.Should().Be("hello world");
+        value.Should().Be(42);
+    }
+}
---
+++ PolyShim/NetCore20/DictionaryEntry.cs
@@ -0,0 +1,20 @@
+﻿#if (NETCOREAPP && !NETCOREAPP2_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System.Collections;
+
+internal static partial class PolyfillExtensions
+{
+    // Used to implement tuple deconstruction, but tuple support is technically not required
+    // https://learn.microsoft.com/en-us/dotnet/api/system.collections.dictionaryentry.deconstruct
+    public static void Deconstruct(this DictionaryEntry entry, out object key, out object? value)
+    {
+        key = entry.Key;
+        value = entry.Value;
+    }
+}
+#endif
