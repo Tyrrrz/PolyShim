4134f966fd7b386b827b4b364905d68c1ad91e10
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-08-07 20:28:57 +0300

    Add `Random.NextBytes(...)` overload for `Span<Memory>`

---
+++ PolyShim.Tests/NetCore21/RandomTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Buffers;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore21;
+
+public class RandomTests
+{
+    [Fact]
+    public void NextBytes_Span_Test()
+    {
+        // Arrange
+        var buffer = ArrayPool<byte>.Shared.Rent(10);
+
+        try
+        {
+            // Act
+            new Random(1234567).NextBytes(buffer.AsSpan());
+
+            // Assert
+            buffer.Should().StartWith(new byte[] { 0x30, 0xE6, 0x63, 0xCA, 0x5F, 0x41, 0x21, 0x48, 0x1F, 0x8F });
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+    }
+}
---
+++ PolyShim/NetCore21/Random.cs
@@ -0,0 +1,22 @@
+﻿#if FEATURE_MEMORY
+#if (NETCOREAPP && !NETCOREAPP2_1_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System;
+
+internal static partial class PolyfillExtensions
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.random.nextbytes#system-random-nextbytes(system-span((system-byte)))
+    public static void NextBytes(this Random random, Span<byte> buffer)
+    {
+        var bufferArray = buffer.ToArray();
+        random.NextBytes(bufferArray);
+        bufferArray.CopyTo(buffer);
+    }
+}
+#endif
+#endif
