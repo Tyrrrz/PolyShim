7e6a6112abf311b55670fb8af64b343f5ebc0319
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-08-07 20:31:26 +0300

    Clean up

--- PolyShim/NetCore21/Random.cs
+++ PolyShim/NetCore21/Random.cs
@@ -1,5 +1,4 @@
-﻿#if FEATURE_MEMORY
-#if (NETCOREAPP && !NETCOREAPP2_1_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
+﻿#if (NETCOREAPP && !NETCOREAPP2_1_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
 #nullable enable
 // ReSharper disable RedundantUsingDirective
 // ReSharper disable CheckNamespace
@@ -10,6 +9,7 @@ using System;
 
 internal static partial class PolyfillExtensions
 {
+#if FEATURE_MEMORY
     // https://learn.microsoft.com/en-us/dotnet/api/system.random.nextbytes#system-random-nextbytes(system-span((system-byte)))
     public static void NextBytes(this Random random, Span<byte> buffer)
     {
@@ -17,6 +17,6 @@ internal static partial class PolyfillExtensions
         random.NextBytes(bufferArray);
         bufferArray.CopyTo(buffer);
     }
-}
 #endif
+}
 #endif
