bfacdc269684c64fd3bffcc442a0b1d382b5f47c
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-12-31 06:17:39 +0200

    Add polyfills for `string.ReplaceLineEndings(...)` methods (#3)

---
+++ PolyShim.Tests/Net60/StringTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net60;
+
+public class StringTests
+{
+    [Fact]
+    public void ReplaceLineEndings_Test()
+    {
+        // Arrange
+        const string str = "abc\r\ndef\rghi\njkl\r\nmno\rpqr\nstu";
+
+        // Act & assert
+        str.ReplaceLineEndings()
+            .Should()
+            .Be(
+                "abc"
+                    + Environment.NewLine
+                    + "def"
+                    + Environment.NewLine
+                    + "ghi"
+                    + Environment.NewLine
+                    + "jkl"
+                    + Environment.NewLine
+                    + "mno"
+                    + Environment.NewLine
+                    + "pqr"
+                    + Environment.NewLine
+                    + "stu"
+            );
+    }
+
+    [Fact]
+    public void ReplaceLineEndings_ReplacementText_Test()
+    {
+        // Arrange
+        const string str = "abc\r\ndef\rghi\njkl\r\nmno\rpqr\nstu";
+
+        // Act & assert
+        str.ReplaceLineEndings(" ").Should().Be("abc def ghi jkl mno pqr stu");
+    }
+}
