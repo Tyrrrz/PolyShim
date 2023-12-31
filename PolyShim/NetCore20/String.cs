586a8eb1f6838eeec6e472e20604d826ac2a98b5
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-12-31 06:00:23 +0200

    Add polyfills for `string.Replace(...)` methods

--- PolyShim/NetCore20/String.cs
+++ PolyShim/NetCore20/String.cs
@@ -6,6 +6,8 @@
 // ReSharper disable PartialTypeWithSinglePart
 
 using System;
+using System.Globalization;
+using System.Text;
 
 internal static partial class PolyfillExtensions
 {
@@ -17,6 +19,77 @@ internal static partial class PolyfillExtensions
 
     // https://learn.microsoft.com/en-us/dotnet/api/system.string.contains#system-string-contains(system-char)
     public static bool Contains(this string str, char c) => str.IndexOf(c) >= 0;
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.string.replace#system-string-replace(system-string-system-string-system-stringcomparison)
+    public static string Replace(
+        this string str,
+        string oldValue,
+        string? newValue,
+        StringComparison comparison
+    )
+    {
+        var buffer = new StringBuilder();
+
+        var index = 0;
+        var lastIndex = 0;
+        while (true)
+        {
+            index = str.IndexOf(oldValue, index, comparison);
+            if (index < 0)
+            {
+                buffer.Append(str, lastIndex, str.Length - lastIndex);
+                break;
+            }
+
+            buffer.Append(str, lastIndex, index - lastIndex);
+            buffer.Append(newValue);
+
+            index += oldValue.Length;
+            lastIndex = index;
+        }
+
+        return buffer.ToString();
+    }
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.string.replace#system-string-replace(system-string-system-string-system-boolean-system-globalization-cultureinfo)
+    public static string Replace(
+        this string str,
+        string oldValue,
+        string? newValue,
+        bool ignoreCase,
+        CultureInfo? culture
+    )
+    {
+        var buffer = new StringBuilder();
+
+        var index = 0;
+        var lastIndex = 0;
+        while (true)
+        {
+            index = (culture ?? CultureInfo.CurrentCulture)
+                .CompareInfo
+                .IndexOf(
+                    str,
+                    oldValue,
+                    index,
+                    ignoreCase ? CompareOptions.IgnoreCase : CompareOptions.None
+                );
+
+            if (index < 0)
+            {
+                buffer.Append(str, lastIndex, str.Length - lastIndex);
+                break;
+            }
+
+            buffer.Append(str, lastIndex, index - lastIndex);
+            buffer.Append(newValue);
+
+            index += oldValue.Length;
+            lastIndex = index;
+        }
+
+        return buffer.ToString();
+    }
 
     // https://learn.microsoft.com/en-us/dotnet/api/system.string.split#system-string-split(system-char-system-int32-system-stringsplitoptions)
     public static string[] Split(
