ea974cfac8e89278b13ccae571dbcd57d5cc8f24
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-12-31 06:21:07 +0200

    Add polyfill for the `string.Contains(char, StringComparison)` method (#4)

--- PolyShim/NetCore21/String.cs
+++ PolyShim/NetCore21/String.cs
@@ -9,6 +9,10 @@ using System;
 
 internal static partial class PolyfillExtensions
 {
+    // https://learn.microsoft.com/en-us/dotnet/api/system.string.contains#system-string-contains(system-char-system-stringcomparison)
+    public static bool Contains(this string str, char c, StringComparison comparison) =>
+        str.Contains(c.ToString(), comparison);
+
     // https://learn.microsoft.com/en-us/dotnet/api/system.string.contains#system-string-contains(system-string-system-stringcomparison)
     public static bool Contains(this string str, string sub, StringComparison comparison) =>
         str.IndexOf(sub, comparison) >= 0;
