3f962794ab2f85747b2e311a76ccdf7b67778fff
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-12-31 06:17:36 +0200

    Add polyfill for the `string.Contains(char, StringComparison)` method

--- PolyShim.Tests/NetCore21/StringTests.cs
+++ PolyShim.Tests/NetCore21/StringTests.cs
@@ -7,7 +7,18 @@ namespace PolyShim.Tests.NetCore21;
 public class StringTests
 {
     [Fact]
-    public void Contains_Test()
+    public void Contains_Char_Test()
+    {
+        // Arrange
+        const string str = "abc";
+
+        // Act & assert
+        str.Contains('B', StringComparison.OrdinalIgnoreCase).Should().BeTrue();
+        str.Contains('B', StringComparison.Ordinal).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Contains_String_Test()
     {
         // Arrange
         const string str = "abc";
