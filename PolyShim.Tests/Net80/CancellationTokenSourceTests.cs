9a024937709dfed5b50403126bff887e17ff08b5
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-01-11 23:12:22 +0200

    Add polyfill for `CancellationTokenSource.CancelAsync()`

---
+++ PolyShim.Tests/Net80/CancellationTokenSourceTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net80;
+
+public class CancellationTokenSourceTests
+{
+    [Fact]
+    public async Task CancelAsync_Test()
+    {
+        // Arrange
+        using var cts = new CancellationTokenSource();
+
+        // Act
+        await cts.CancelAsync();
+
+        // Assert
+        cts.IsCancellationRequested.Should().BeTrue();
+    }
+}
