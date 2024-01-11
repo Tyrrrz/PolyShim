07574cdd259d86a0b9c00a0c37fc77a3f26af898
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-01-11 23:19:10 +0200

    Add polyfill for `CancellationTokenSource.CancelAsync()` (#5)

---
+++ PolyShim/Net80/CancellationTokenSource.cs
@@ -0,0 +1,30 @@
+﻿#if FEATURE_TASK
+#if (NETCOREAPP && !NET8_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal static partial class PolyfillExtensions
+{
+    // https://learn.microsoft.com/en-us/dotnet/api/system.threading.cancellationtokensource.cancelasync
+    public static Task CancelAsync(this CancellationTokenSource cts)
+    {
+#if (NETFRAMEWORK && !NET45_OR_GREATER)
+        return Task.Factory.StartNew(() => cts.Cancel());
+#elif (NETFRAMEWORK && !NET46_OR_GREATER) || (NETSTANDARD && !NETSTANDARD1_3_OR_GREATER)
+        cts.Cancel();
+        return Task.FromResult<object?>(null);
+#else
+        cts.Cancel();
+        return Task.CompletedTask;
+#endif
+    }
+}
+#endif
+#endif
