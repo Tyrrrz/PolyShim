73d1129bea097eb9b2c338dd916aeab421fafaea
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-02-01 20:37:56 +0200

    Include the polyfill for `ExcludeFromCodeCoverageAttribute` in a wider range of frameworks because its targets were updated at some point

--- PolyShim/NetCore20/ExcludeFromCodeCoverageAttribute.cs
+++ PolyShim/NetCore20/ExcludeFromCodeCoverageAttribute.cs
@@ -1,4 +1,7 @@
-﻿#if (NETCOREAPP && !NETCOREAPP2_0_OR_GREATER) || (NETFRAMEWORK && !NET40_OR_GREATER) || (NETSTANDARD && !NETSTANDARD2_0_OR_GREATER)
+﻿// Even though this attribute was added in .NET Core 2.0, the attribute targets were updated a few versions later.
+// So in order to provide a consistent API surface, we need to include this polyfill in a wider range of versions.
+//#if (NETCOREAPP && !NETCOREAPP2_0_OR_GREATER) || (NETFRAMEWORK && !NET40_OR_GREATER) || (NETSTANDARD && !NETSTANDARD2_0_OR_GREATER)
+#if (NETCOREAPP && !NETCOREAPP2_1_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
 #nullable enable
 // ReSharper disable RedundantUsingDirective
 // ReSharper disable CheckNamespace
