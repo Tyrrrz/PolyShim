60900cd7b7cf6e4fc81b66d9852effca951da6af
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-05-22 19:36:43 +0300

    Add polyfills for `RequiresDynamicCodeAttribute`, `RequiresUnreferencedCodeAttribute`, and `UnconditionalSuppressMessageAttribute` (#7)

---
+++ PolyShim/Net50/UnconditionalSuppressMessageAttribute.cs
@@ -0,0 +1,27 @@
+﻿#if (NETCOREAPP && !NET5_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+namespace System.Diagnostics.CodeAnalysis;
+
+// https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis.unconditionalsuppressmessageattribute
+[AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+[ExcludeFromCodeCoverage]
+internal class UnconditionalSuppressMessageAttribute(string category, string checkId) : Attribute
+{
+    public string Category { get; } = category;
+
+    public string CheckId { get; } = checkId;
+
+    public string? MessageId { get; init; }
+
+    public string? Justification { get; init; }
+
+    public string? Scope { get; init; }
+
+    public string? Target { get; init; }
+}
+#endif
