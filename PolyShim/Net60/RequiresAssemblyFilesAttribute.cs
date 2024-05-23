b2488e8ae366ab571e70e8159c8f75982b8fcbdb
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-05-23 23:36:36 +0300

    Add polyfill for `RequiresAssemblyFilesAttribute`

---
+++ PolyShim/Net60/RequiresAssemblyFilesAttribute.cs
@@ -0,0 +1,26 @@
+﻿#if (NETCOREAPP && !NET6_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+namespace System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(
+    AttributeTargets.Constructor
+        | AttributeTargets.Event
+        | AttributeTargets.Method
+        | AttributeTargets.Property,
+    Inherited = false
+)]
+internal class RequiresAssemblyFilesAttribute(string? message) : Attribute
+{
+    public RequiresAssemblyFilesAttribute()
+        : this(null) { }
+
+    public string? Message { get; } = message;
+
+    public string? Url { get; init; }
+}
+#endif
