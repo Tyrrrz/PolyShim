feeff1a10eafb98f0735f745b13542b7e00be8c0
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-09-30 21:05:24 +0300

    Add tests

--- PolyShim.Tests/Net50/TypeTests.cs
+++ PolyShim.Tests/Net50/TypeTests.cs
@@ -5,15 +5,23 @@ namespace PolyShim.Tests.Net50;
 
 public class TypeTests
 {
-    private class BaseClass;
+    private interface IBaseInterface;
+
+    private class BaseClass : IBaseInterface;
 
     private class DerivedClass : BaseClass;
 
     [Fact]
     public void IsAssignableTo_Test()
     {
-        typeof(DerivedClass).IsAssignableTo(typeof(BaseClass)).Should().BeTrue();
         typeof(DerivedClass).IsAssignableTo(typeof(DerivedClass)).Should().BeTrue();
+        typeof(DerivedClass).IsAssignableTo(typeof(BaseClass)).Should().BeTrue();
+        typeof(DerivedClass).IsAssignableTo(typeof(IBaseInterface)).Should().BeTrue();
+        typeof(BaseClass).IsAssignableTo(typeof(BaseClass)).Should().BeTrue();
         typeof(BaseClass).IsAssignableTo(typeof(DerivedClass)).Should().BeFalse();
+        typeof(BaseClass).IsAssignableTo(typeof(IBaseInterface)).Should().BeTrue();
+        typeof(IBaseInterface).IsAssignableTo(typeof(IBaseInterface)).Should().BeTrue();
+        typeof(IBaseInterface).IsAssignableTo(typeof(DerivedClass)).Should().BeFalse();
+        typeof(IBaseInterface).IsAssignableTo(typeof(BaseClass)).Should().BeFalse();
     }
 }
--- PolyShim.Tests/NetCore20/TypeTests.cs
+++ PolyShim.Tests/NetCore20/TypeTests.cs
@@ -5,15 +5,37 @@ namespace PolyShim.Tests.NetCore20;
 
 public class TypeTests
 {
-    private class BaseClass;
+    private interface IBaseInterface;
+
+    private class BaseClass : IBaseInterface;
 
     private class DerivedClass : BaseClass;
 
     [Fact]
+    public void IsAssignableFrom_Test()
+    {
+        typeof(DerivedClass).IsAssignableFrom(typeof(DerivedClass)).Should().BeTrue();
+        typeof(DerivedClass).IsAssignableFrom(typeof(BaseClass)).Should().BeFalse();
+        typeof(DerivedClass).IsAssignableFrom(typeof(IBaseInterface)).Should().BeFalse();
+        typeof(BaseClass).IsAssignableFrom(typeof(BaseClass)).Should().BeTrue();
+        typeof(BaseClass).IsAssignableFrom(typeof(DerivedClass)).Should().BeTrue();
+        typeof(BaseClass).IsAssignableFrom(typeof(IBaseInterface)).Should().BeFalse();
+        typeof(IBaseInterface).IsAssignableFrom(typeof(IBaseInterface)).Should().BeTrue();
+        typeof(IBaseInterface).IsAssignableFrom(typeof(DerivedClass)).Should().BeTrue();
+        typeof(IBaseInterface).IsAssignableFrom(typeof(BaseClass)).Should().BeTrue();
+    }
+
+    [Fact]
     public void IsSubclassOf_Test()
     {
-        typeof(DerivedClass).IsSubclassOf(typeof(BaseClass)).Should().BeTrue();
         typeof(DerivedClass).IsSubclassOf(typeof(DerivedClass)).Should().BeFalse();
+        typeof(DerivedClass).IsSubclassOf(typeof(BaseClass)).Should().BeTrue();
+        typeof(DerivedClass).IsSubclassOf(typeof(IBaseInterface)).Should().BeFalse();
+        typeof(BaseClass).IsSubclassOf(typeof(BaseClass)).Should().BeFalse();
         typeof(BaseClass).IsSubclassOf(typeof(DerivedClass)).Should().BeFalse();
+        typeof(BaseClass).IsSubclassOf(typeof(IBaseInterface)).Should().BeFalse();
+        typeof(IBaseInterface).IsSubclassOf(typeof(IBaseInterface)).Should().BeFalse();
+        typeof(IBaseInterface).IsSubclassOf(typeof(DerivedClass)).Should().BeFalse();
+        typeof(IBaseInterface).IsSubclassOf(typeof(BaseClass)).Should().BeFalse();
     }
 }
