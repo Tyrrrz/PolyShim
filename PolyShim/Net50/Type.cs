c0cf1d51daf38cf3a7ca6d9f2321aa425515e43c
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-09-30 20:54:41 +0300

    Add polyfill for `Type.IsAssignableFrom(...)`

--- PolyShim/Net50/Type.cs
+++ PolyShim/Net50/Type.cs
@@ -12,10 +12,6 @@ internal static partial class PolyfillExtensions
 {
     // https://learn.microsoft.com/en-us/dotnet/api/system.type.isassignableto
     public static bool IsAssignableTo(this Type type, Type? otherType) =>
-#if NETSTANDARD && !NETSTANDARD2_0_OR_GREATER
-        otherType?.GetTypeInfo().IsAssignableFrom(type.GetTypeInfo()) == true;
-#else
         otherType?.IsAssignableFrom(type) == true;
-#endif
 }
 #endif
