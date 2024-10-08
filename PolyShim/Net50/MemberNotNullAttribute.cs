9acb98d1280d68f90b7a878b8fed1143a13816fa
Brian Friesen <brian.friesen@gmail.com>
2024-10-08 15:44:34 -0400

    Move and fix [MemberNotNull] & [MemberNotNullWhen]
    According to the documentation, these attributes weren't added until .NET 5.
    
    - https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis.membernotnullattribute?view=net-8.0#applies-to
    - https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis.membernotnullwhenattribute?view=net-8.0#applies-to

--- PolyShim/Net50/MemberNotNullAttribute.cs
+++ PolyShim/Net50/MemberNotNullAttribute.cs
@@ -1,4 +1,4 @@
-﻿#if (NETCOREAPP && !NETCOREAPP3_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
+﻿#if (NETCOREAPP && !NET5_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
 #nullable enable
 // ReSharper disable RedundantUsingDirective
 // ReSharper disable CheckNamespace
