0e4433aa8d1ac7cb4ff52267a7a8136d2f53817b
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-11-14 02:26:25 +0200

    Add polyfill for `OverloadResolutionPriorityAttribute` (#19)

---
+++ PolyShim.Tests/Net90/OverloadResolutionPriorityAttributeTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net90;
+
+public class OverloadResolutionPriorityAttributeTests
+{
+    [OverloadResolutionPriority(1)]
+    private int Foo(int value = 0) => value;
+
+    private int Foo() => 42;
+
+    [Fact]
+    public void Initialization_Test()
+    {
+        // Act & assert
+        Foo().Should().Be(0);
+    }
+}
