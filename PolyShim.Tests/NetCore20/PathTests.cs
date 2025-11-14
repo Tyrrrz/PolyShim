2498df4df3ca81a7888d3ec23a5a959ca4dd3def
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-15 01:25:53 +0200

    Add polyfill for `Path.GetRelativePath(...)`

---
+++ PolyShim.Tests/NetCore20/PathTests.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore20;
+
+public class PathTests
+{
+    [Fact]
+    public void GetRelativePath_Test()
+    {
+        // Act & assert
+        Path.GetRelativePath("/a/b/c", "/a/b/c/d/e.txt").Should().Be(Path.Combine("d", "e.txt"));
+
+        Path.GetRelativePath("/a/b/c", "/a/b/x/y/z.txt")
+            .Should()
+            .Be(Path.Combine("..", "x", "y", "z.txt"));
+
+        Path.GetRelativePath("C:\\a\\b\\c", "C:\\a\\b\\c\\d\\e.txt")
+            .Should()
+            .Be(Path.Combine("d", "e.txt"));
+
+        Path.GetRelativePath("C:\\a\\b\\c", "C:\\a\\b\\x\\y\\z.txt")
+            .Should()
+            .Be(Path.Combine("..", "x", "y", "z.txt"));
+    }
+}
