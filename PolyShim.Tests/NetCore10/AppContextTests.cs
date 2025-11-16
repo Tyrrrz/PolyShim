638749b22935501c07e5c99f9343cd55b8017373
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 17:52:15 +0200

    Add polyfill for `AppContext.TargetFrameworkName` and `TargetFrameworkAttribute`

---
+++ PolyShim.Tests/NetCore10/AppContextTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore10;
+
+public class AppContextTests
+{
+    [Fact]
+    public void TargetFrameworkName_Test()
+    {
+        // Arrange
+        var frameworkName = (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly())
+            .GetCustomAttribute<TargetFrameworkAttribute>()
+            ?.FrameworkName;
+
+        // Act & assert
+        AppContext.TargetFrameworkName.Should().NotBeNullOrWhiteSpace();
+        AppContext.TargetFrameworkName.Should().Be(frameworkName);
+    }
+}
