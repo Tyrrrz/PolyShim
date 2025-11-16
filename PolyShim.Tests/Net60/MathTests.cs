1a67dbed6dfcd73e3ddfc174d2e0de92088d516f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 18:38:18 +0200

    Add polyfills for `Math.Clamp(...)`

---
+++ PolyShim.Tests/Net60/MathTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net60;
+
+public class MathTests
+{
+    [Fact]
+    public void Clamp_Test()
+    {
+        // Act & assert
+        Math.Clamp((nint)5, (nint)1, (nint)10).Should().Be((nint)5);
+        Math.Clamp((nint)0, (nint)1, (nint)10).Should().Be((nint)1);
+        Math.Clamp((nint)15, (nint)1, (nint)10).Should().Be((nint)10);
+    }
+}
