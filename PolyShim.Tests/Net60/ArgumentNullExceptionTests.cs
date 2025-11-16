3867d0bc8c20526f953e4c76a0201ba104ffca81
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 18:25:44 +0200

    Add polyfills for `ArgumentNullException.ThrowIfNull(...)`, `ArgumentException.ThrowIfNullOrEmpty(...)` and `ArgumentException.ThrowIfNullOrWhiteSpace(...)`

---
+++ PolyShim.Tests/Net60/ArgumentNullExceptionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net60;
+
+public class ArgumentNullExceptionTests
+{
+    [Fact]
+    public void ThrowIfNull_Test()
+    {
+        // Arrange
+        const string notNullValue = "Hello, World!";
+        const string? nullValue = null;
+
+        // Act & assert
+        ArgumentNullException.ThrowIfNull(notNullValue);
+
+        var ex = Assert.Throws<ArgumentNullException>(() =>
+            ArgumentNullException.ThrowIfNull(nullValue)
+        );
+
+        ex.ParamName.Should().Be(nameof(nullValue));
+    }
+}
