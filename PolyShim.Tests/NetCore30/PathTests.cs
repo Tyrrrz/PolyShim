11c2d22e1b3c83a66688b97d950454cb6da5c692
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 18:51:43 +0200

    Add polyfills for `Path.EndsInDirectorySeparator(...)` and `Path.TrimEndingDirectorySeparator(...)`

---
+++ PolyShim.Tests/NetCore30/PathTests.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore30;
+
+public class PathTests
+{
+    [Fact]
+    public void EndsInDirectorySeparator_Test()
+    {
+        // Act & assert
+        Path.EndsInDirectorySeparator("C:\\Folder\\Subfolder\\").Should().BeTrue();
+        Path.EndsInDirectorySeparator("C:/Folder/Subfolder/").Should().BeTrue();
+        Path.EndsInDirectorySeparator("C:\\Folder\\Subfolder").Should().BeFalse();
+        Path.EndsInDirectorySeparator("C:/Folder/Subfolder").Should().BeFalse();
+        Path.EndsInDirectorySeparator("/a/b/c/").Should().BeTrue();
+        Path.EndsInDirectorySeparator("/a/b/c").Should().BeFalse();
+        Path.EndsInDirectorySeparator(string.Empty).Should().BeFalse();
+    }
+
+    [Fact]
+    public void TrimEndingDirectorySeparator_Test()
+    {
+        // Act & assert
+        Path.TrimEndingDirectorySeparator("C:\\Folder\\Subfolder\\")
+            .Should()
+            .Be("C:\\Folder\\Subfolder");
+        Path.TrimEndingDirectorySeparator("C:/Folder/Subfolder/")
+            .Should()
+            .Be("C:/Folder/Subfolder");
+        Path.TrimEndingDirectorySeparator("C:\\Folder\\Subfolder")
+            .Should()
+            .Be("C:\\Folder\\Subfolder");
+        Path.TrimEndingDirectorySeparator("C:/Folder/Subfolder").Should().Be("C:/Folder/Subfolder");
+        Path.TrimEndingDirectorySeparator("/a/b/c/").Should().Be("/a/b/c");
+        Path.TrimEndingDirectorySeparator("/a/b/c").Should().Be("/a/b/c");
+        Path.TrimEndingDirectorySeparator(string.Empty).Should().Be(string.Empty);
+    }
+}
