581127b27ae0dca550604a30f3417d1ba953fecb
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 19:58:33 +0200

    Fix doc link inconsistency

--- PolyShim/NetCore21/DateTimeOffset.cs
+++ PolyShim/NetCore21/DateTimeOffset.cs
@@ -11,7 +11,7 @@ internal static partial class PolyfillExtensions2
 {
     extension(DateTimeOffset)
     {
-        // https://learn.microsoft.com/en-us/dotnet/api/system.datetimeoffset.unixepoch?view=net-9.0
+        // https://learn.microsoft.com/dotnet/api/system.datetimeoffset.unixepoch
         public static DateTimeOffset UnixEpoch => new(1970, 1, 1, 0, 0, 0, TimeSpan.Zero);
     }
 }
