87c96c9be5ba866a006e04d55a0d9258eb1bbf1b
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 16:34:53 +0200

    Add polyfills for some of the `OperatingSystem.IsXyz()` methods

---
+++ PolyShim.Tests/Net50/OperatingSystemTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net50;
+
+public class OperatingSystemTests
+{
+    [Fact]
+    public void IsXyz_Test()
+    {
+        // Act
+        var isLinux = OperatingSystem.IsLinux();
+        var isMacOs = OperatingSystem.IsMacOS();
+        var isWindows = OperatingSystem.IsWindows();
+
+        // Assert
+#if PLATFORM_WINDOWS
+        isLinux.Should().BeFalse();
+        isMacOs.Should().BeFalse();
+        isWindows.Should().BeTrue();
+#elif PLATFORM_LINUX
+        isLinux.Should().BeTrue();
+        isMacOs.Should().BeFalse();
+        isWindows.Should().BeFalse();
+#elif PLATFORM_OSX
+        isLinux.Should().BeFalse();
+        isMacOs.Should().BeTrue();
+        isWindows.Should().BeFalse();
+#else
+        isLinux.Should().BeFalse();
+        isMacOs.Should().BeFalse();
+        isWindows.Should().BeFalse();
+#endif
+    }
+}
