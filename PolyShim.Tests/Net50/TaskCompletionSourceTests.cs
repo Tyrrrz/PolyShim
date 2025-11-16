76d4b0740686ecfb9849c26ba99cf97fc2374e95
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 16:06:16 +0200

    Add polyfill for `TaskCompletionSource<T>.SetCanceled(...)`

--- PolyShim.Tests/Net50/TaskCompletionSourceTests.cs
+++ PolyShim.Tests/Net50/TaskCompletionSourceTests.cs
@@ -4,42 +4,38 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Xunit;
 
-namespace PolyShim.Tests.NetCore10;
+namespace PolyShim.Tests.Net50;
 
 public class TaskCompletionSourceTests
 {
     [Fact]
-    public async Task TrySetCanceled_Test()
+    public async Task SetCanceled_Test()
     {
         // Arrange
         var cancellationToken = new CancellationToken(true);
         var tcs = new TaskCompletionSource<int>();
 
         // Act
-        var result = tcs.TrySetCanceled(cancellationToken);
+        tcs.SetCanceled(cancellationToken);
         var ex = await Assert.ThrowsAnyAsync<OperationCanceledException>(async () =>
             await tcs.Task
         );
 
         // Assert
-        result.Should().BeTrue();
         tcs.Task.IsCanceled.Should().BeTrue();
         ex.CancellationToken.Should().Be(cancellationToken);
         ex.CancellationToken.IsCancellationRequested.Should().BeTrue();
     }
 
     [Fact]
-    public void TrySetCanceled_AlreadyCompleted_Test()
+    public void SetCanceled_AlreadyCompleted_Test()
     {
         // Arrange
         var cancellationToken = new CancellationToken(true);
         var tcs = new TaskCompletionSource<int>();
         tcs.SetResult(42);
 
-        // Act
-        var result = tcs.TrySetCanceled(cancellationToken);
-
-        // Assert
-        result.Should().BeFalse();
+        // Act & assert
+        Assert.ThrowsAny<InvalidOperationException>(() => tcs.SetCanceled(cancellationToken));
     }
 }
