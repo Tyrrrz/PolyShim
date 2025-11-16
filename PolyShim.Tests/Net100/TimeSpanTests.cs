6d16dd41b9073a5f31e81803ea391c9fba887235
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 23:24:41 +0200

    Add polyfill for `TimeSpan.FromMilliseconds(long, [long])`

---
+++ PolyShim.Tests/Net100/TimeSpanTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net100;
+
+public class TimeSpanTests
+{
+    [Fact]
+    public void FromMilliseconds_Test()
+    {
+        // Act & assert
+        TimeSpan.FromMilliseconds(1234).Should().Be(TimeSpan.FromSeconds(1.234));
+    }
+}
---
+++ PolyShim.Tests/Net90/TimeSpanTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net90;
+
+public class TimeSpanTests
+{
+    [Fact]
+    public void FromMilliseconds_Test()
+    {
+        // Act & assert
+        TimeSpan.FromMilliseconds(1234, 567).Should().Be(TimeSpan.FromSeconds(1.234567));
+    }
+}
