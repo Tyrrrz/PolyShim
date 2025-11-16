dfee9d3f2374ab1290d3bb22ee7de16941a4df1f
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2025-11-16 17:33:01 +0200

    Add polyfills for `Task.IsCompletedSuccessfully` and `TaskCompletionSource` (non-generic) (#35)

---
+++ PolyShim.Tests/NetCore20/TaskTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.NetCore20;
+
+public class TaskTests
+{
+    [Fact]
+    public void IsCompletedSuccessfully_Test()
+    {
+        // Arrange
+        var tcs1 = new TaskCompletionSource<int>();
+        var tcs2 = new TaskCompletionSource<int>();
+
+        tcs1.SetResult(10);
+        tcs2.SetException(new Exception());
+
+        // Act & assert
+        tcs1.Task.IsCompletedSuccessfully.Should().BeTrue();
+        tcs2.Task.IsCompletedSuccessfully.Should().BeFalse();
+    }
+}
