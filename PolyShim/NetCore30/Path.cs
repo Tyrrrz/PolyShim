5aaa9b371ab232ce16ea3888d0f2b92e1643d6f6
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2025-11-17 15:30:24 +0200

    Add polyfill for `Path.Join(...)` (#44)

--- PolyShim/NetCore30/Path.cs
+++ PolyShim/NetCore30/Path.cs
@@ -6,26 +6,23 @@
 // ReSharper disable PartialTypeWithSinglePart
 
 using System.IO;
+using System.Linq;
+using System.Text;
 
 internal static partial class PolyfillExtensions
 {
     extension(Path)
     {
-        // https://learn.microsoft.com/dotnet/api/system.io.path.endsindirectoryseparator#system-io-path-endsindirectoryseparator(system-string)
-        public static bool EndsInDirectorySeparator(string? path)
-        {
-            if (string.IsNullOrEmpty(path))
-                return false;
-
-            var lastChar = path![^1];
-
+        private static bool IsDirectorySeparator(char c) =>
 #if !NETSTANDARD || NETSTANDARD1_3_OR_GREATER
-            return lastChar == Path.DirectorySeparatorChar
-                || lastChar == Path.AltDirectorySeparatorChar;
+            c == Path.DirectorySeparatorChar || c == Path.AltDirectorySeparatorChar;
 #else
-            return lastChar is '\\' or '/';
+            c is '\\' or '/';
 #endif
-        }
+
+        // https://learn.microsoft.com/dotnet/api/system.io.path.endsindirectoryseparator#system-io-path-endsindirectoryseparator(system-string)
+        public static bool EndsInDirectorySeparator(string? path) =>
+            !string.IsNullOrEmpty(path) && Path.IsDirectorySeparator(path![^1]);
 
         // https://learn.microsoft.com/dotnet/api/system.io.path.trimendingdirectoryseparator#system-io-path-trimendingdirectoryseparator(system-string)
         public static string TrimEndingDirectorySeparator(string path) =>
@@ -33,6 +30,49 @@ internal static partial class PolyfillExtensions
             && !Path.GetPathRoot(path).Equals(path, System.StringComparison.Ordinal)
                 ? path[..^1]
                 : path;
+
+        // https://learn.microsoft.com/dotnet/api/system.io.path.join#system-io-path-join(system-string())
+        public static string Join(params string?[] paths)
+        {
+            if (paths.Length <= 0)
+                return string.Empty;
+
+            var maxSize = paths.Sum(path => path?.Length ?? 0);
+            maxSize += paths.Length - 1;
+
+            var builder = new StringBuilder(maxSize);
+
+            foreach (var path in paths)
+            {
+                if (string.IsNullOrEmpty(path))
+                    continue;
+
+                if (builder.Length == 0)
+                {
+                    builder.Append(path);
+                }
+                else
+                {
+                    if (
+                        !Path.IsDirectorySeparator(builder[^1])
+                        && !Path.IsDirectorySeparator(path![0])
+                    )
+                    {
+                        builder.Append(
+#if !NETSTANDARD || NETSTANDARD1_3_OR_GREATER
+                            Path.DirectorySeparatorChar
+#else
+                            '/'
+#endif
+                        );
+                    }
+
+                    builder.Append(path);
+                }
+            }
+
+            return builder.ToString();
+        }
     }
 }
 #endif
