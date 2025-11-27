7d446a14b2d6d8eaf88f95a0c338082353ee0b2a
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-27 17:16:59 +0200

    Add polyfill for `EqualityComparer<T>.Create(...)`

---
+++ PolyShim.Tests/Net80/EqualityComparerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net80;
+
+public class EqualityComparerTests
+{
+    [Fact]
+    public void Create_Test()
+    {
+        // Arrange
+        var comparer = EqualityComparer<int>.Create(
+            (x, y) => Math.Abs(x) == Math.Abs(y),
+            x => Math.Abs(x).GetHashCode()
+        );
+
+        // Act & Assert
+        comparer.Equals(5, 5).Should().BeTrue();
+        comparer.Equals(5, -5).Should().BeTrue();
+        comparer.Equals(5, 4).Should().BeFalse();
+        comparer.GetHashCode(5).Should().Be(comparer.GetHashCode(-5));
+    }
+
+    [Fact]
+    public void Create_NoGetHashCode_Test()
+    {
+        // Arrange
+        var comparer = EqualityComparer<int>.Create((x, y) => Math.Abs(x) == Math.Abs(y));
+
+        // Act & Assert
+        comparer.Equals(5, 5).Should().BeTrue();
+        comparer.Equals(5, -5).Should().BeTrue();
+        comparer.Equals(5, 4).Should().BeFalse();
+        Assert.Throws<NotSupportedException>(() =>
+            comparer.GetHashCode(5).Should().Be(comparer.GetHashCode(-5))
+        );
+    }
+}
---
+++ PolyShim/Net80/EqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿#if (NETCOREAPP && !NET8_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System;
+using System.Collections.Generic;
+
+internal static partial class PolyfillExtensions
+{
+    private class DelegateEqualityComparer<T>(Func<T?, T?, bool> equals, Func<T, int>? getHashCode)
+        : EqualityComparer<T>
+    {
+        public override bool Equals(T? x, T? y) => equals(x, y);
+
+        public override int GetHashCode(T obj) =>
+            getHashCode is not null ? getHashCode(obj) : throw new NotSupportedException();
+    }
+
+    extension<T>(EqualityComparer<T>)
+    {
+        // https://learn.microsoft.com/dotnet/api/system.collections.generic.equalitycomparer-1.create
+        public static EqualityComparer<T> Create(
+            Func<T?, T?, bool> equals,
+            Func<T, int>? getHashCode = null
+        ) => new DelegateEqualityComparer<T>(equals, getHashCode);
+    }
+}
+#endif
