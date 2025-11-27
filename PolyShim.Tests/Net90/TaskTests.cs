fff75c2c846126c90f21adea94657baa5976a4c8
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-27 16:42:40 +0200

    Fix concurrency issues in tests

--- PolyShim.Tests/Net90/TaskTests.cs
+++ PolyShim.Tests/Net90/TaskTests.cs
@@ -16,25 +16,26 @@ public class TaskTests
         var tcs2 = new TaskCompletionSource();
         var tcs3 = new TaskCompletionSource();
 
-        // Act
-        var iterationTask = Task.Run(async () =>
+        var completedTasks = new List<Task>();
+
+        void CompleteNext()
         {
-            var tasks = new List<Task>();
-            await foreach (var completedTask in Task.WhenEach(tcs1.Task, tcs2.Task, tcs3.Task))
-            {
-                tasks.Add(completedTask);
-            }
+            // Complete tasks in a specific order
+            if (completedTasks.Count == 0)
+                tcs2.SetResult();
+            else if (completedTasks.Count == 1)
+                tcs1.SetResult();
+            else if (completedTasks.Count == 2)
+                tcs3.SetResult();
+        }
 
-            return tasks;
-        });
-
-        tcs2.SetResult();
-        await Task.Delay(10);
-        tcs1.SetResult();
-        await Task.Delay(10);
-        tcs3.SetResult();
-
-        var completedTasks = await iterationTask;
+        // Act
+        CompleteNext();
+        await foreach (var completedTask in Task.WhenEach(tcs1.Task, tcs2.Task, tcs3.Task))
+        {
+            completedTasks.Add(completedTask);
+            CompleteNext();
+        }
 
         // Assert
         completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
@@ -62,64 +63,32 @@ public class TaskTests
         var tcs2 = new TaskCompletionSource();
         var tcs3 = new TaskCompletionSource();
 
-        // Act
-        var iterationTask = Task.Run(async () =>
+        var completedTasks = new List<Task>();
+
+        void CompleteNext()
         {
-            var tasks = new List<Task>();
-            await foreach (var completedTask in Task.WhenEach(tcs1.Task, tcs2.Task, tcs3.Task))
-            {
-                tasks.Add(completedTask);
-            }
+            // Complete tasks in a specific order
+            if (completedTasks.Count == 0)
+                tcs2.SetException(new InvalidOperationException("Test exception"));
+            else if (completedTasks.Count == 1)
+                tcs1.SetResult();
+            else if (completedTasks.Count == 2)
+                tcs3.SetResult();
+        }
 
-            return tasks;
-        });
-
-        tcs2.SetException(new InvalidOperationException("Test exception"));
-        await Task.Delay(10);
-        tcs1.SetResult();
-        await Task.Delay(10);
-        tcs3.SetResult();
-
-        var completedTasks = await iterationTask;
+        // Act
+        CompleteNext();
+        await foreach (var completedTask in Task.WhenEach(tcs1.Task, tcs2.Task, tcs3.Task))
+        {
+            completedTasks.Add(completedTask);
+            CompleteNext();
+        }
 
         // Assert
         completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
         await Assert.ThrowsAsync<InvalidOperationException>(async () => await completedTasks[0]);
         await completedTasks[1];
         await completedTasks[2];
-    }
-
-    [Fact(Timeout = 5000)]
-    public async Task WhenEach_AlreadyPartiallyCompleted_Test()
-    {
-        // Arrange
-        var tcs1 = new TaskCompletionSource();
-        var tcs2 = new TaskCompletionSource();
-        var tcs3 = new TaskCompletionSource();
-
-        tcs2.SetResult();
-
-        // Act
-        var iterationTask = Task.Run(async () =>
-        {
-            var tasks = new List<Task>();
-            await foreach (var completedTask in Task.WhenEach(tcs1.Task, tcs2.Task, tcs3.Task))
-            {
-                tasks.Add(completedTask);
-            }
-
-            return tasks;
-        });
-
-        await Task.Delay(10);
-        tcs1.SetResult();
-        await Task.Delay(10);
-        tcs3.SetResult();
-
-        var completedTasks = await iterationTask;
-
-        // Assert
-        completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
     }
 
     [Fact(Timeout = 5000)]
@@ -130,31 +99,28 @@ public class TaskTests
         var tcs2 = new TaskCompletionSource<int>();
         var tcs3 = new TaskCompletionSource<int>();
 
-        // Act
-        var iterationTask = Task.Run(async () =>
+        var completedTasks = new List<Task<int>>();
+
+        void CompleteNext()
         {
-            var tasks = new List<Task<int>>();
+            // Complete tasks in a specific order
+            if (completedTasks.Count == 0)
+                tcs2.SetResult(11);
+            else if (completedTasks.Count == 1)
+                tcs1.SetResult(22);
+            else if (completedTasks.Count == 2)
+                tcs3.SetResult(33);
+        }
 
-            // For some reason, dropping the array literal leads to call ambiguity on .NET Framework
-            // ReSharper disable RedundantExplicitParamsArrayCreation
-            await foreach (
-                var completedTask in Task.WhenEach(new[] { tcs1.Task, tcs2.Task, tcs3.Task })
-            )
-            {
-                tasks.Add(completedTask);
-            }
-            // ReSharper restore RedundantExplicitParamsArrayCreation
-
-            return tasks;
-        });
-
-        tcs2.SetResult(11);
-        await Task.Delay(10);
-        tcs1.SetResult(22);
-        await Task.Delay(10);
-        tcs3.SetResult(33);
-
-        var completedTasks = await iterationTask;
+        // Act
+        CompleteNext();
+        await foreach (
+            var completedTask in Task.WhenEach(new[] { tcs1.Task, tcs2.Task, tcs3.Task })
+        )
+        {
+            completedTasks.Add(completedTask);
+            CompleteNext();
+        }
 
         // Assert
         completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
@@ -185,77 +151,32 @@ public class TaskTests
         var tcs2 = new TaskCompletionSource<int>();
         var tcs3 = new TaskCompletionSource<int>();
 
-        // Act
-        var iterationTask = Task.Run(async () =>
+        var completedTasks = new List<Task<int>>();
+
+        void CompleteNext()
         {
-            var tasks = new List<Task<int>>();
+            // Complete tasks in a specific order
+            if (completedTasks.Count == 0)
+                tcs2.SetException(new InvalidOperationException("Test exception"));
+            else if (completedTasks.Count == 1)
+                tcs1.SetResult(22);
+            else if (completedTasks.Count == 2)
+                tcs3.SetResult(33);
+        }
 
-            // For some reason, dropping the array literal leads to call ambiguity on .NET Framework
-            // ReSharper disable RedundantExplicitParamsArrayCreation
-            await foreach (
-                var completedTask in Task.WhenEach(new[] { tcs1.Task, tcs2.Task, tcs3.Task })
-            )
-            {
-                tasks.Add(completedTask);
-            }
-            // ReSharper restore RedundantExplicitParamsArrayCreation
-
-            return tasks;
-        });
-
-        tcs2.SetException(new InvalidOperationException("Test exception"));
-        await Task.Delay(10);
-        tcs1.SetResult(22);
-        await Task.Delay(10);
-        tcs3.SetResult(33);
-
-        var completedTasks = await iterationTask;
+        // Act
+        CompleteNext();
+        await foreach (
+            var completedTask in Task.WhenEach(new[] { tcs1.Task, tcs2.Task, tcs3.Task })
+        )
+        {
+            completedTasks.Add(completedTask);
+            CompleteNext();
+        }
 
         // Assert
         completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
         await Assert.ThrowsAsync<InvalidOperationException>(async () => await completedTasks[0]);
-        (await completedTasks[1]).Should().Be(22);
-        (await completedTasks[2]).Should().Be(33);
-    }
-
-    [Fact(Timeout = 5000)]
-    public async Task WhenEach_Result_AlreadyPartiallyCompleted_Test()
-    {
-        // Arrange
-        var tcs1 = new TaskCompletionSource<int>();
-        var tcs2 = new TaskCompletionSource<int>();
-        var tcs3 = new TaskCompletionSource<int>();
-
-        tcs2.SetResult(11);
-
-        // Act
-        var iterationTask = Task.Run(async () =>
-        {
-            var tasks = new List<Task<int>>();
-
-            // For some reason, dropping the array literal leads to call ambiguity on .NET Framework
-            // ReSharper disable RedundantExplicitParamsArrayCreation
-            await foreach (
-                var completedTask in Task.WhenEach(new[] { tcs1.Task, tcs2.Task, tcs3.Task })
-            )
-            {
-                tasks.Add(completedTask);
-            }
-            // ReSharper restore RedundantExplicitParamsArrayCreation
-
-            return tasks;
-        });
-
-        await Task.Delay(10);
-        tcs1.SetResult(22);
-        await Task.Delay(10);
-        tcs3.SetResult(33);
-
-        var completedTasks = await iterationTask;
-
-        // Assert
-        completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
-        (await completedTasks[0]).Should().Be(11);
         (await completedTasks[1]).Should().Be(22);
         (await completedTasks[2]).Should().Be(33);
     }
