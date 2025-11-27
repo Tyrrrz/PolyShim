ed1cee68b7883c1123c10871c11fed9a8808958d
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-27 15:54:27 +0200

    Partially completed

--- PolyShim.Tests/Net90/TaskTests.cs
+++ PolyShim.Tests/Net90/TaskTests.cs
@@ -91,6 +91,39 @@ public class TaskTests
     }
 
     [Fact(Timeout = 5000)]
+    public async Task WhenEach_AlreadyPartiallyCompleted_Test()
+    {
+        // Arrange
+        var tcs1 = new TaskCompletionSource();
+        var tcs2 = new TaskCompletionSource();
+        var tcs3 = new TaskCompletionSource();
+
+        tcs2.SetResult();
+
+        // Act
+        var iterationTask = Task.Run(async () =>
+        {
+            var tasks = new List<Task>();
+            await foreach (var completedTask in Task.WhenEach(tcs1.Task, tcs2.Task, tcs3.Task))
+            {
+                tasks.Add(completedTask);
+            }
+
+            return tasks;
+        });
+
+        await Task.Delay(10);
+        tcs1.SetResult();
+        await Task.Delay(10);
+        tcs3.SetResult();
+
+        var completedTasks = await iterationTask;
+
+        // Assert
+        completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
+    }
+
+    [Fact(Timeout = 5000)]
     public async Task WhenEach_Result_Test()
     {
         // Arrange
@@ -182,6 +215,48 @@ public class TaskTests
         // Assert
         completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
         await Assert.ThrowsAsync<InvalidOperationException>(async () => await completedTasks[0]);
+        (await completedTasks[1]).Should().Be(22);
+        (await completedTasks[2]).Should().Be(33);
+    }
+
+    [Fact(Timeout = 5000)]
+    public async Task WhenEach_Result_AlreadyPartiallyCompleted_Test()
+    {
+        // Arrange
+        var tcs1 = new TaskCompletionSource<int>();
+        var tcs2 = new TaskCompletionSource<int>();
+        var tcs3 = new TaskCompletionSource<int>();
+
+        tcs2.SetResult(11);
+
+        // Act
+        var iterationTask = Task.Run(async () =>
+        {
+            var tasks = new List<Task<int>>();
+
+            // For some reason, dropping the array literal leads to call ambiguity on .NET Framework
+            // ReSharper disable RedundantExplicitParamsArrayCreation
+            await foreach (
+                var completedTask in Task.WhenEach(new[] { tcs1.Task, tcs2.Task, tcs3.Task })
+            )
+            {
+                tasks.Add(completedTask);
+            }
+            // ReSharper restore RedundantExplicitParamsArrayCreation
+
+            return tasks;
+        });
+
+        await Task.Delay(10);
+        tcs1.SetResult(22);
+        await Task.Delay(10);
+        tcs3.SetResult(33);
+
+        var completedTasks = await iterationTask;
+
+        // Assert
+        completedTasks.Should().Equal(tcs2.Task, tcs1.Task, tcs3.Task);
+        (await completedTasks[0]).Should().Be(11);
         (await completedTasks[1]).Should().Be(22);
         (await completedTasks[2]).Should().Be(33);
     }
