3daf267ecf280c5729ca24bb94195e8168633421
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2025-11-30 18:00:52 +0200

    Add polyfills for `Convert.ToHexString[Lower](...)` and `Convert.FromHexString(...)` (#59)

---
+++ PolyShim.Tests/Net50/ConvertTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PolyShim.Tests.Net50;
+
+public class ConvertTests
+{
+    [Fact]
+    public void FromHexString_Test()
+    {
+        // Act & assert
+        Convert.FromHexString("4A6F686E").Should().Equal("John"u8.ToArray());
+        Convert.FromHexString("4a6f686e").Should().Equal("John"u8.ToArray());
+        Convert.FromHexString("4A6f686E").Should().Equal("John"u8.ToArray());
+        Convert.FromHexString("").Should().BeEmpty();
+        Assert.Throws<FormatException>(() => Convert.FromHexString("4A6F686")); // Odd length
+        Assert.Throws<FormatException>(() => Convert.FromHexString("4A6F68GH")); // Invalid character
+    }
+
+    [Fact]
+    public void ToHexString_Test()
+    {
+        // Act & assert
+        Convert.ToHexString("John"u8.ToArray()).Should().Be("4A6F686E");
+        Convert.ToHexString([]).Should().Be("");
+        Convert.ToHexString("John"u8.ToArray(), 1, 2).Should().Be("6F68");
+    }
+}
---
+++ PolyShim/Net90/Convert.cs
@@ -0,0 +1,23 @@
+﻿#if (NETCOREAPP && !NET9_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System;
+
+internal static partial class PolyfillExtensions
+{
+    extension(Convert)
+    {
+        // https://learn.microsoft.com/dotnet/api/system.convert.tohexstringlower#system-convert-tohexstringlower(system-byte()-system-int32-system-int32)
+        public static string ToHexStringLower(byte[] value, int startIndex, int length) =>
+            Convert.ToHexString(value, startIndex, length).ToLowerInvariant();
+
+        // https://learn.microsoft.com/dotnet/api/system.convert.tohexstringlower#system-convert-tohexstringlower(system-byte())
+        public static string ToHexStringLower(byte[] value) =>
+            ToHexStringLower(value, 0, value.Length);
+    }
+}
+#endif
