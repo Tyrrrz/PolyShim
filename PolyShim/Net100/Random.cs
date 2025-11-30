9be1c49684f1785f621e619a68366980d90ae0c8
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-11-30 18:32:50 +0200

    Add polyfills for `Random.NextInt64(...)`, `Random.NextSingle(...)`, `Random.GetItems(...)`, `Random.Shuffle(...)`, `Random.GetHexString(...)`, `Random.GetString(...)`

---
+++ PolyShim/Net100/Random.cs
@@ -0,0 +1,47 @@
+﻿#if (NETCOREAPP && !NET10_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal static partial class PolyfillExtensions
+{
+    extension(Random random)
+    {
+        // https://learn.microsoft.com/dotnet/api/system.random.gethexstring#system-random-gethexstring(system-int32-system-boolean)
+        public string GetHexString(int stringLength, bool lowercase = false)
+        {
+            var bytes = new byte[(stringLength + 1) / 2];
+            random.NextBytes(bytes);
+
+            return lowercase ? Convert.ToHexStringLower(bytes) : Convert.ToHexString(bytes);
+        }
+
+        // Signature-compatible replacement for GetString(ReadOnlySpan<char>, int)
+        // https://learn.microsoft.com/dotnet/api/system.random.getstring
+        public string GetString(char[] choices, int length)
+        {
+            var buffer = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+            {
+                var index = random.Next(choices.Length);
+                buffer.Append(choices[index]);
+            }
+
+            return buffer.ToString();
+        }
+
+#if FEATURE_MEMORY
+        // https://learn.microsoft.com/dotnet/api/system.random.getstring
+        public string GetString(ReadOnlySpan<char> choices, int length) =>
+            random.GetString(choices.ToArray(), length);
+#endif
+    }
+}
+#endif
---
+++ PolyShim/Net80/Random.cs
@@ -0,0 +1,44 @@
+﻿#if (NETCOREAPP && !NET8_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System;
+using System.Collections.Generic;
+
+internal static partial class PolyfillExtensions
+{
+    extension(Random random)
+    {
+        // https://learn.microsoft.com/dotnet/api/system.random.getitems#system-random-getitems-1(-0()-system-int32)
+        public T[] GetItems<T>(T[] choices, int length)
+        {
+            var result = new T[length];
+            var selectedIndices = new HashSet<int>();
+
+            while (selectedIndices.Count < length)
+            {
+                var index = random.Next(choices.Length);
+                if (selectedIndices.Add(index))
+                {
+                    result[selectedIndices.Count - 1] = choices[index];
+                }
+            }
+
+            return result;
+        }
+
+        // https://learn.microsoft.com/dotnet/api/system.random.shuffle#system-random-shuffle-1(-0())
+        public void Shuffle<T>(T[] items)
+        {
+            for (var i = items.Length - 1; i > 0; i--)
+            {
+                var j = random.Next(i + 1);
+                (items[i], items[j]) = (items[j], items[i]);
+            }
+        }
+    }
+}
+#endif
