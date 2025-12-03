d5c29493719a4e530c4e24a0bb56809695148667
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-12-03 16:44:13 +0200

    Add polyfills for `IntPtr.[Try]Parse(...)`, `UIntPtr.[Try]Parse(...)`

---
+++ PolyShim/Net50/IntPtr.cs
@@ -0,0 +1,49 @@
+﻿#if (NETCOREAPP && !NET5_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
+#nullable enable
+// ReSharper disable RedundantUsingDirective
+// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+// ReSharper disable PartialTypeWithSinglePart
+
+using System;
+using System.Globalization;
+
+internal static partial class PolyfillExtensions2
+{
+    extension(IntPtr)
+    {
+        // https://learn.microsoft.com/dotnet/api/system.intptr.parse#system-intptr-parse(system-string-system-globalization-numberstyles-system-iformatprovider)
+        public static IntPtr Parse(string s, NumberStyles style, IFormatProvider? provider) =>
+            new(IntPtr.Size == 4 ? int.Parse(s, style, provider) : long.Parse(s, style, provider));
+
+        // https://learn.microsoft.com/dotnet/api/system.intptr.parse#system-intptr-parse(system-string-system-iformatprovider)
+        public static IntPtr Parse(string s, IFormatProvider? provider) =>
+            new(IntPtr.Size == 4 ? int.Parse(s, provider) : long.Parse(s, provider));
+
+        // https://learn.microsoft.com/dotnet/api/system.intptr.parse#system-intptr-parse(system-string-system-globalization-numberstyles)
+        public static IntPtr Parse(string s, NumberStyles style) =>
+            new(IntPtr.Size == 4 ? int.Parse(s, style) : long.Parse(s, style));
+
+        // https://learn.microsoft.com/dotnet/api/system.intptr.parse#system-intptr-parse(system-string)
+        public static IntPtr Parse(string s) =>
+            new(IntPtr.Size == 4 ? int.Parse(s) : long.Parse(s));
+
+        // https://learn.microsoft.com/dotnet/api/system.intptr.tryparse#system-intptr-tryparse(system-string-system-intptr@)
+        public static bool TryParse(string? s, out IntPtr result)
+        {
+            if (IntPtr.Size == 4)
+            {
+                var success = int.TryParse(s, out var intResult);
+                result = new IntPtr(intResult);
+                return success;
+            }
+            else
+            {
+                var success = long.TryParse(s, out var longResult);
+                result = new IntPtr(longResult);
+                return success;
+            }
+        }
+    }
+}
+#endif
