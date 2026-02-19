8a9074fcbb33e69c941f603b8c4736b64011adfd
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2026-02-19 16:08:10 +0200

    Remove unnecessary conditionals

--- PolyShim/NetCore10/Task.cs
+++ PolyShim/NetCore10/Task.cs
@@ -166,8 +166,6 @@ internal static class MemberPolyfills_NetCore10_Task
 #endif
 
 #if NETFRAMEWORK && !NET45_OR_GREATER
-        // Timer is not available on .NET Standard 1.0 and 1.1
-#if !(NETSTANDARD && !NETSTANDARD1_2_OR_GREATER)
         // https://learn.microsoft.com/dotnet/api/system.threading.tasks.task.delay#system-threading-tasks-task-delay(system-timespan-system-threading-cancellationtoken)
         public static Task Delay(TimeSpan delay, CancellationToken cancellationToken)
         {
@@ -210,7 +208,6 @@ internal static class MemberPolyfills_NetCore10_Task
 
         // https://learn.microsoft.com/dotnet/api/system.threading.tasks.task.delay#system-threading-tasks-task-delay(system-timespan)
         public static Task Delay(TimeSpan delay) => Task.Delay(delay, CancellationToken.None);
-#endif
 #endif
     }
 }
