1f09abd8555d4efa07aac6f72d8a2f6038cda53d
tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2026-04-05 14:48:08 +0300

    Fix more tests on netfx

--- PolyShim.Tests/NetCore30/TextWriterTests.cs
+++ PolyShim.Tests/NetCore30/TextWriterTests.cs
@@ -11,17 +11,21 @@ public class TextWriterTests
     public async Task DisposeAsync_Test()
     {
         // Arrange
+        const string expectedText = "Hello, World!";
         using var innerStream = new MemoryStream();
 
         // Use a buffered stream to ensure flushing data is required
         using var stream = new BufferedStream(innerStream, bufferSize: 4096);
         using var writer = new StreamWriter(stream);
-        await writer.WriteAsync("Hello, World!");
+        await writer.WriteAsync(expectedText);
+
+        // This has to be called before Dispose() because that resets the Encoding
+        var expectedBytes = writer.Encoding.GetBytes(expectedText);
 
         // Act
         await writer.DisposeAsync();
 
         // Assert
-        innerStream.ToArray().Should().Equal(writer.Encoding.GetBytes("Hello, World!"));
+        innerStream.ToArray().Should().Equal(expectedBytes);
     }
 }
