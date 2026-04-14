d99d4b60695fb78f694828a8ffcf9c0582a1603a
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-14 20:00:30 +0300

    Update TextWriter.cs

--- PolyShim/Net80/TextWriter.cs
+++ PolyShim/Net80/TextWriter.cs
@@ -1,4 +1,3 @@
-﻿#if FEATURE_TASK
 #if (NETCOREAPP && !NET8_0_OR_GREATER) || (NETFRAMEWORK) || (NETSTANDARD)
 #nullable enable
 #pragma warning disable CS0436
@@ -19,13 +18,14 @@ internal static class MemberPolyfills_Net80_TextWriter
 {
     extension(TextWriter writer)
     {
+﻿#if FEATURE_TASK
         // https://learn.microsoft.com/dotnet/api/system.io.textwriter.flushasync#system-io-textwriter-flushasync(system-threading-cancellationtoken)
         public Task FlushAsync(CancellationToken cancellationToken)
         {
             cancellationToken.ThrowIfCancellationRequested();
             return writer.FlushAsync();
         }
+#endif
     }
 }
-#endif
 #endif
